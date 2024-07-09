8f490b6945649102f26fb33db0ceff1b0d868aa4
raviavaiya3 <21bmiit007@gmail.com>
2024-07-09 15:27:50 +0530

    lecture 1

---
+++ Lecture_1.py
@@ -0,0 +1,14 @@
+
+# How to print statements in python......
+
+print("Hello World")
+print("This is my first python program....")
+print(50)
+print(25.99)
+
+# ----------------
+
+# Variable 
+    # A Variable is a name given to a memory location in a program......
+
+    
