8761e71e4bae5b495316332971d9d70cd21c9179
raviavaiya <raviavaiya03@gmail.com>
2025-04-18 17:06:24 +0530

    file

---
+++ Problems/file_operation.py
@@ -0,0 +1,19 @@
+# f = open("D:/Python-Learning/Python-Learning/Problems/demo.txt","r") # open file in read mode
+# data = f.read() # read the file
+# print(data) # read the file again
+
+
+# line1 = f.readline() # read the first line
+# line2 = f.read() # read the second line 
+# print(line1) # print the first line
+
+# f.close() # close the file
+
+# f = open("D:/Python-Learning/Python-Learning/Problems/demo.txt","w") # open file in write mode
+# f.write("Hello World") # write to the file
+# print(f)
+# f.close()
+
+f = open("D:/Python-Learning/Python-Learning/Problems/demo.txt","a") # open file in append mode
+f.write("Hello Ravi") # write to the file
+
